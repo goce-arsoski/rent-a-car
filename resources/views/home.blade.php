71404a8f1a174fbb328efd7913d93601d8ff0f54
goce-arsoski <goce_ars@yahoo.com>
2022-03-16 00:07:11 +0100

    Initial commit and view for home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<main class="sm:container sm:mx-auto sm:mt-10">
+  <div class="container p-5">
+        <div class="card mb-5 p-5 b-rad">
+            <h1 class="text-center"><b>WELCOME TO OUR CARS BOOKING SITE</b></h1>
+        </div>
+        <div class="align-middle">
+            <button class="btn btn-warning">Bookings</button>
+            <button class="btn btn-info">People</button>
+            <button class="btn btn-primary">Cars</button>
+        </div>
+    </div>
+</main>
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,18 @@
+<div class="container">
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <strong><a class="navbar-brand">Car booking</a></strong>
+    <a class="navbar-brand"></a><a class="btn btn-dark" href="/">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+        <a class="btn" href="/cars">Book Car</a>
+        <a class="btn" href="/cars/new">Add Car</a>
+        <a class="btn" href="/people/new">Add Person</a>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+      <li class="mobile-menu">
+      </li>
+      </div>
+    </div>
+  </nav>
+</div>
